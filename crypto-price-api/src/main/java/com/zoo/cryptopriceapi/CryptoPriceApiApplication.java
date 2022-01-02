eeca5e4d88c450d39c6b67fd2d43886b4795e3a0
Alwin V. John <alwinvjohn7@gmail.com>
2022-01-02 04:45:12 -0500

    project skelton updated

---
+++ crypto-price-api/src/main/java/com/zoo/cryptopriceapi/CryptoPriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.zoo.cryptopriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPriceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPriceApiApplication.class, args);
+	}
+
+}
---
+++ crypto-price-api/src/test/java/com/zoo/cryptopriceapi/CryptoPriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo.cryptopriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
