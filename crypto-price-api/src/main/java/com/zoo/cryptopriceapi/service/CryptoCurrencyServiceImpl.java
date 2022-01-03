f8546c1cf39d8d6676397de558e4cfee3c33bfb3
Alwin V. John <alwinvjohn7@gmail.com>
2022-01-03 00:25:27 -0500

    log ip address for debugging

--- crypto-price-api/src/main/java/com/zoo/cryptopriceapi/service/CryptoCurrencyServiceImpl.java
+++ crypto-price-api/src/main/java/com/zoo/cryptopriceapi/service/CryptoCurrencyServiceImpl.java
@@ -7,6 +7,7 @@ import com.zoo.cryptopriceapi.exception.BusinessException;
 import com.zoo.cryptopriceapi.exception.BusinessExceptionReason;
 import com.zoo.cryptopriceapi.repository.CryptoCurrencyRepository;
 import com.zoo.cryptopriceapi.util.CommonUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class CryptoCurrencyServiceImpl implements CryptoCurrencyService {
 
     private CryptoCurrencyRepository currencyRepository;
@@ -39,6 +41,7 @@ public class CryptoCurrencyServiceImpl implements CryptoCurrencyService {
                         , c.getUnitPriceCurrencyCode(), c.getUnitPrice())).collect(Collectors.toList());
 
         String ip = InetAddress.getLocalHost().getHostAddress();
+        log.debug("current IP of the system ", ip);
         return new SupportedCryptoCurrencyListDto(ip, CommonUtil.getCurrentTimeStamp(), currencies);
     }
 }
