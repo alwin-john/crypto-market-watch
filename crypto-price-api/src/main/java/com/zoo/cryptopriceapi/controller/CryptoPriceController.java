88edc4923384a54eb048d2af69cc287c81d88f94
Alwin V. John <alwinvjohn7@gmail.com>
2022-01-03 15:12:56 -0500

    allowed cross origin requests

--- crypto-price-api/src/main/java/com/zoo/cryptopriceapi/controller/CryptoPriceController.java
+++ crypto-price-api/src/main/java/com/zoo/cryptopriceapi/controller/CryptoPriceController.java
@@ -6,6 +6,7 @@ import com.zoo.cryptopriceapi.service.CryptoCurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/crypto/api/v1")
+@CrossOrigin("*")
 public class CryptoPriceController {
 
     private CryptoCurrencyService cryptoCurrencyService;
