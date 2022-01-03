0291ba99b798f4bc6f39b8c9d93e446a59899b87
Alwin V. John <alwinvjohn7@gmail.com>
2022-01-03 15:15:21 -0500

    Data added to initialization script

--- crypto-price-api/src/main/resources/data.sql
+++ crypto-price-api/src/main/resources/data.sql
@@ -1,3 +1,5 @@
 
-insert into CRYPTO_CURRENCY_INFO values (1, '127.0.0.1', 'BITCOIN', 'Bitcoin','USD'
-                                                         ,'&dollar', 67, CURRENT_TIMESTAMP());
+insert into CRYPTO_CURRENCY_INFO values (1, '127.0.0.1', 'BITCOIN', 'Bitcoin','USD','$', 100.89, CURRENT_TIMESTAMP());
+
+insert into CRYPTO_CURRENCY_INFO values (2, '127.0.1.1', 'ETHER', 'Ether','EURO','£', 67.456, CURRENT_TIMESTAMP());
+
