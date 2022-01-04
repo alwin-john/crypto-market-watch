2fad2e872510501dd846d68195eab9f202717a99
Alwin V. John <alwinvjohn7@gmail.com>
2022-01-04 06:21:38 -0500

    Reverting back to old url since the dockeraized app throwing cors error

--- crypto-market-frontend-service/src/http-common.js
+++ crypto-market-frontend-service/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://backend/zoo',
+    baseURL: 'http://localhost:8080/zoo',
     headers: {
         'Content-Type': 'application/json'
     }
