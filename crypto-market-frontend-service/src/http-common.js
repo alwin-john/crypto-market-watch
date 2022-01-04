21ce93013875283adb66a7434bffe25a1c00e4e2
Alwin V. John <alwinvjohn7@gmail.com>
2022-01-04 06:19:22 -0500

    Adding docker files to containeraize the application

--- crypto-market-frontend-service/src/http-common.js
+++ crypto-market-frontend-service/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/zoo',
+    baseURL: 'http://backend/zoo',
     headers: {
         'Content-Type': 'application/json'
     }
---
+++ crypto-price-api/src/main/java/com/zoo/cryptopriceapi/config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.zoo.cryptopriceapi.config;
+
+public class webconfig {
+}
