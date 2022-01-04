2c3c89f973f943c77f68ab229a103a9ba5046349
Alwin V. John <alwinvjohn7@gmail.com>
2022-01-04 07:19:09 -0500

    Adding more data to script

--- crypto-price-api/src/main/resources/data.sql
+++ crypto-price-api/src/main/resources/data.sql
@@ -1,5 +1,8 @@
 
-insert into CRYPTO_CURRENCY_INFO values (1, '127.0.0.1', 'BITCOIN', 'Bitcoin','USD','$', 100.89, CURRENT_TIMESTAMP());
+insert into CRYPTO_CURRENCY_INFO values (1, '127.0.0.1', 'BITCOIN', 'Bitcoin','USD','$', 100123.89, CURRENT_TIMESTAMP());
 
 insert into CRYPTO_CURRENCY_INFO values (2, '127.0.1.1', 'ETHER', 'Ether','EURO','£', 67.456, CURRENT_TIMESTAMP());
 
+insert into CRYPTO_CURRENCY_INFO values (3, '127.0.1.2', 'BINANCE', 'Binance Coin' ,'EURO','£', 8569.09, CURRENT_TIMESTAMP());
+
+insert into CRYPTO_CURRENCY_INFO values (4, '127.0.1.3', 'TETHER', 'Tether','USD','$', 67789.456, CURRENT_TIMESTAMP());
